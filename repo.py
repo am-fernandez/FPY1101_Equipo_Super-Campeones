0300b24f70d8d136d9b3229576ea7fff44fff531
am-fernandez <am.fernandezf@duocuc.cl>
2025-06-23 11:44:58 -0400

    Update repo.py

--- repo.py
+++ repo.py
@@ -11,7 +11,8 @@ while True:
         print("Programa finalizado.")
         break
     elif op == "1":
-        pass # Aquí se llamará a la función del integrante 1
+        pass def datos_Aman():
+            print("Mi nombre es Amán Fernández y tengo 23 años 👁️👄👁️")
     elif op == "2":
         pass # Aquí se llamará a la función del integrante 2
     elif op == "3":
