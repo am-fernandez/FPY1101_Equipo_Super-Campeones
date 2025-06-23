79bcbadf685c043605244cc00c109949d091066e
am-fernandez <am.fernandezf@duocuc.cl>
2025-06-23 11:26:48 -0400

    Update repo.py

--- repo.py
+++ repo.py
@@ -16,5 +16,7 @@ while True:
         pass # Aquí se llamará a la función del integrante 2
     elif op == "3":
         pass # Aquí se llamará a la función del integrante 3
+    elif op == "4":
+        pass # Aquí se llamará a la función del integrante 4
     else:
         print(" Opción inválida.")
